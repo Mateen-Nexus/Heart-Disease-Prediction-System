62fe368c647f2dc06a33ec96bcfaaf8e8174b8d1
Abdul Mateen <abdulmateen7017@gmail.com>
2026-07-02 22:02:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,13 @@ bp = st.number_input("Blood Pressure", min_value=50, max_value=250, value=120)
 chol = st.number_input("Cholesterol", min_value=100, max_value=600, value=200)
 
 if st.button("Predict"):
-
+  
     sex_val = 1 if sex == "Male" else 0
 
-
-    if age > 50 or bp > 130 or chol > 240:
+  
+    if bp > 140 or chol > 240 or age > 60:
         st.error("⚠️ High Risk of Heart Disease")
     else:
         st.success("✅ Low Risk of Heart Disease")
+
+
